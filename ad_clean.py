58a685190ad5bb0a8cbe2935aa2d6559c63c051c
jw <jeb.williams8@gmail.com>
2021-01-12 15:38:09 +1000

    complete Albecht name modification

--- ad_clean.py
+++ ad_clean.py
@@ -1,4 +1,30 @@
 import glob
-image_list = sorted(glob.glob("./Artworks/resized/resized/Albrecht*"))
+import os
+import re
+from shutil import copy2
 
-print(image_list[0], image_list[-1])
+
+path = os.getcwd()
+image_list = sorted(glob.glob(path + "/Artworks/resized/resized/Albrecht*"))
+
+path_ar = path + '/Artworks/resized/resized/archive'
+
+
+# try:
+#     os.mkdir(path_ar)
+# except OSError:
+#     print ("Creation of the directory %s failed" % path_ar)
+# else:
+#     print ("Successfully created the directory %s " % path_ar)
+
+# for ad_image in image_list:
+#     path_copy = path_ar + ad_image[71:]
+#     copy2(ad_image, path_copy)
+
+
+ad_pattern = re.compile(r'(?=Albrecht_Du).*?(?=\d)')
+
+for ad_image in image_list:
+
+    path_mod = re.sub(ad_pattern, "Albrecht_Durer_", ad_image)
+    os.rename(ad_image, path_mod)
