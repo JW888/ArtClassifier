b71e25589bb637845f158d90740e2c82b6c64895
jw <jeb.williams8@gmail.com>
2021-01-14 16:04:03 +1000

    set up labels for images

--- art_class_cnn.py
+++ art_class_cnn.py
@@ -1,1 +1,17 @@
 from PIL import Image
+import glob
+import os
+
+path = os.getcwd()
+image_list = sorted(glob.glob(path + "/Artworks/resized/resized/*.jpg"))
+
+
+image_data = {}
+
+
+for image in image_list:
+    im = Image.open(image)
+    w, h = im.size
+    image_data[image] = {'width' : w, 'height' : h}
+
+print(image_data[image_list[0]])
